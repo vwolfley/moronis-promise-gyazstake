7e8a00ca5525ed4581d2c767d7169aa52c90493c
vern wolfley <vern.wolfley@gmail.com>
2024-04-21 16:21:19 -0700

    add clothing gallery info

--- src/components/sections/ClothingGallery.jsx
+++ src/components/sections/ClothingGallery.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import Costume1 from "../../images/clothing/mp-costume1.webp"
 import Costume7 from "../../images/clothing/mp-clothing-group.webp"
-
+import Costume2 from "../../images/clothing/mp-group1.webp"
+import Costume3 from "../../images/clothing/tunics.webp"
 
 function ClothingGallery() {
   return (
@@ -25,7 +26,20 @@ function ClothingGallery() {
             />
           </div>
         </div>
-      
+        <div className="flex w-1/2 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block w-full rounded-lg object-cover object-center"
+              src={Costume2}
+            />
+          </div>
+        </div>
+        <div className="flex w-1/2 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+          <iframe className="block w-full h-full rounded-lg object-cover object-center" src="https://www.youtube.com/embed/YKTf1EJ5M5k?si=7DfbVzSQcU_nxnma" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div>
       </div>
     </section>
   )
